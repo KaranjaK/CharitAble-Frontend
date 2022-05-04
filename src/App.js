0b8d297cef4d8fef97a2f343a89b771ea0254645
KK <karanjakelvin77@gmail.com>
2022-05-04 04:34:53 +0300

    It will configure imports

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import './App.css';
-
-function Header(props){
-  return(
-    <h1>Charitable</h1>
-  )
-}
-
-function Main(props){
-  return(
-    <section></section>
-  )
-}
-
-function Footer(props){
-  return(
-    <p>Copyright {props.year}</p>
-  )
-}
-
-const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-]
-
-const dayObjects = days.map((day, i) => ({id: i, title: day}));
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Header adjective='awesome'/>
-      <Main name='a blast' days={dayObjects}/>
-      <Footer year={new Date().getFullYear()}/>
-      
-    </div>
-  );
+
+  const [emotions, setEmotions] = useState('Happy')
+  return(
+      <div className='App'>
+       
+      </div>
+
+  )
 }
 
 export default App;

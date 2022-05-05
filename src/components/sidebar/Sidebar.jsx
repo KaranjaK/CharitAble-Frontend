7a7cfb3255ca6081edb90001215bb3674d6e4a2f
KK <karanjakelvin77@gmail.com>
2022-05-05 15:04:15 +0300

    It will update the sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './sidebar.css'
+import logo from '..//..//assets/images/logo.png'
 
 export const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <div className='top'>Logo</div>
+      <div className='top'>
+        <img src={logo} alt="The page logo" className="logo" />
+      </div>
       <div className='body'>List</div>
       <div className='footer'>Theme</div>
     </div>
--- src/pages/adminhome/Adminhome.jsx
+++ src/pages/adminhome/Adminhome.jsx
@@ -6,9 +6,7 @@ import { Sidebar } from '../../components/sidebar/Sidebar'
 export const Adminhome = () => {
   return (
     <div className='navbar'>
-      <Navbar />
       <Sidebar />
-      <div className='na'></div>
     </div>
   )
 }

54d61785029bcb25c647bc8d93a2f442dd657cae
KK <karanjakelvin77@gmail.com>
2022-05-08 08:59:46 +0300

    It will introduce a new component called widgets

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,8 @@
+import './widget.css'
+
+export const Widget = () => {
+  return (
+    <div className='widget'>Widget</div>
+    
+  )
+}
--- src/pages/adminhome/Adminhome.jsx
+++ src/pages/adminhome/Adminhome.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './adminhome.css'
 import { Navbar } from '../../components/navbar/Navbar'
 import { Sidebar } from '../../components/sidebar/Sidebar'
+import { Widget } from '../../components/widget/Widget'
 
 const Adminhome = () => {
   return (
     <div className='admin'>
       <Sidebar />
       <Navbar />
+      <div className="body">
+        <Widget />
+      </div>
     </div>
   )
 }

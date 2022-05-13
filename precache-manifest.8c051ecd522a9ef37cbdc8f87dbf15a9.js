564232d9ddcb0d6bcc454dc8c709b670e345a941
KK <karanjakelvin77@gmail.com>
2022-05-13 11:17:15 +0300

    Updates

---
+++ precache-manifest.8c051ecd522a9ef37cbdc8f87dbf15a9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "c03573d69e7a13a10002",
+    "url": "/CharitAble-Frontend/static/css/main.2439b3f1.chunk.css"
+  },
+  {
+    "revision": "c03573d69e7a13a10002",
+    "url": "/CharitAble-Frontend/static/js/main.2897cb55.chunk.js"
+  },
+  {
+    "revision": "f16ab57cabe95211c356",
+    "url": "/CharitAble-Frontend/static/js/runtime~main.755fb91e.js"
+  },
+  {
+    "revision": "85a5335b650793eb65bb",
+    "url": "/CharitAble-Frontend/static/css/2.5e0e1665.chunk.css"
+  },
+  {
+    "revision": "85a5335b650793eb65bb",
+    "url": "/CharitAble-Frontend/static/js/2.29e754b3.chunk.js"
+  },
+  {
+    "revision": "a77399c0fb8c992124e72860cb62477a",
+    "url": "/CharitAble-Frontend/static/media/charity.a77399c0.jpg"
+  },
+  {
+    "revision": "0daf46e41ea94a6be8b507ac7f31bcc1",
+    "url": "/CharitAble-Frontend/static/media/charity2.0daf46e4.jpg"
+  },
+  {
+    "revision": "8dd17e142d19e367148d7641242f9653",
+    "url": "/CharitAble-Frontend/static/media/logo.8dd17e14.png"
+  },
+  {
+    "revision": "2781bebd94fb56a35bcac4fb0b18c7d0",
+    "url": "/CharitAble-Frontend/static/media/background.2781bebd.jpg"
+  },
+  {
+    "revision": "fa353ce74d9bb29778ef1b678ad0e1d4",
+    "url": "/CharitAble-Frontend/static/media/char5.fa353ce7.jpeg"
+  },
+  {
+    "revision": "5a1c41bf06b104f2ebf297c3d65cefe3",
+    "url": "/CharitAble-Frontend/static/media/char6.5a1c41bf.jpeg"
+  },
+  {
+    "revision": "d516cba896ac815556bb101e3b7060fd",
+    "url": "/CharitAble-Frontend/static/media/char7.d516cba8.jpeg"
+  },
+  {
+    "revision": "7cab3d7edf5947eb4821df339d67ca3a",
+    "url": "/CharitAble-Frontend/static/media/char8.7cab3d7e.jpeg"
+  },
+  {
+    "revision": "5056bdb792476a9d7296c715cdd1f1be",
+    "url": "/CharitAble-Frontend/static/media/char9.5056bdb7.jpeg"
+  },
+  {
+    "revision": "5c24a393bc71b0ffc3e4b60aeda3c5cc",
+    "url": "/CharitAble-Frontend/static/media/char10.5c24a393.jpeg"
+  },
+  {
+    "revision": "02ccabb957b347eb2066a4e9c2c4e3b4",
+    "url": "/CharitAble-Frontend/static/media/char11.02ccabb9.jpeg"
+  },
+  {
+    "revision": "0302fa0f786b18f6ec31c4ee5caee20a",
+    "url": "/CharitAble-Frontend/static/media/char12.0302fa0f.jpeg"
+  },
+  {
+    "revision": "6f5b4831a1de89476018cb2a89e92d0c",
+    "url": "/CharitAble-Frontend/static/media/char15.6f5b4831.jpeg"
+  },
+  {
+    "revision": "617a4ead0fe94c7b7eb0abe21217a1ca",
+    "url": "/CharitAble-Frontend/static/media/char16.617a4ead.jpeg"
+  },
+  {
+    "revision": "8d360a83627fcd42e9859d3fd02e2b4e",
+    "url": "/CharitAble-Frontend/static/media/char17.8d360a83.jpeg"
+  },
+  {
+    "revision": "8ac48af3daaa855feb6072bf684e9fd4",
+    "url": "/CharitAble-Frontend/static/media/char18.8ac48af3.jpeg"
+  },
+  {
+    "revision": "b82d5554e199165ba56e2edfb8378743",
+    "url": "/CharitAble-Frontend/static/media/char19.b82d5554.jpeg"
+  },
+  {
+    "revision": "18bb085da71f8f12c2cbc4d3873a8f4f",
+    "url": "/CharitAble-Frontend/static/media/char20.18bb085d.jpeg"
+  },
+  {
+    "revision": "69ace1c5478481e0865f16ec34d14d99",
+    "url": "/CharitAble-Frontend/static/media/partner.69ace1c5.jpg"
+  },
+  {
+    "revision": "f2c896abe09a5627357462409b13a993",
+    "url": "/CharitAble-Frontend/index.html"
+  }
+];
